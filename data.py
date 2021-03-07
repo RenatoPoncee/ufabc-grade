e39f9c12e1b9a1bb294a5b28f839245de19216b9
RenatoPoncee <renato.poncee@gmail.com>
2021-03-06 22:17:02 -0300

    criando site

---
+++ data.py
@@ -0,0 +1,22 @@
+def Materias():
+    materias = [
+        {
+            'id': 1,
+            'codigo': 'JFEI-89',
+            'nome': 'Bases Matemáticas',
+            'recomendadas': '',
+        },
+        {
+            'id': 2,
+            'codigo': 'ASDF-75',
+            'nome': 'Função de uma variável',
+            'recomendadas': '',
+        },
+        {
+            'id': 3,
+            'codigo': 'QWER-25',
+            'nome': 'Função de várias variáveis',
+            'recomendadas': '',
+        }
+    ]
+    return materias
